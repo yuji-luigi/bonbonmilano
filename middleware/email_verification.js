4789748d690d9d508b54dd7de792a10091a1eda9
Yuji Sato <yujisato@192.168.1.4>
2021-10-04 07:18:41 +0200

    updated gmail settings and prod dev variables

--- middleware/email_verification.js
+++ middleware/email_verification.js
@@ -3,36 +3,37 @@
    been used to register.
    I should switch to one token auth. But here I leave it like this. */
 
-const nodemailer = require('nodemailer')
-const Token = require('../models/Token')
-const User = require('../models/User')
-const crypto = require('crypto')
+const nodemailer = require("nodemailer");
+const Token = require("../models/Token");
+const User = require("../models/User");
+const crypto = require("crypto");
 
 const sendVerifyEmail = async (req, res, next) => {
-try{
-  const {username, email, cap, tel, via, citta, _id} = req.user
-  const authLinkHost = req.authLinkHost
-  const authLink = req.authLink
-  const msg = req.msg
-  const email_header = req.email_header
-  
+  try {
+    const { username, email, cap, tel, via, citta, _id } = req.user;
+    let authLinkHost = req.authLinkHost;
+    if (process.env.NODE_ENV !== "production") {
+      authLinkHost = "http://localhost:4242";
+    }
 
-  res.clearCookie('token')
-  await Token.deleteOne({_userId: _id})
+    const authLink = req.authLink;
+    const msg = req.msg;
+    const email_header = req.email_header;
+    console.log(authLinkHost);
+    res.clearCookie("token");
+    await Token.deleteOne({ _userId: _id });
 
-  const tokenSchema = new Token(
-    {
+    const tokenSchema = new Token({
       _userId: _id,
-      params_token: crypto.randomBytes(16).toString('hex'),
-      cookie_token: crypto.randomBytes(16).toString('hex')
-    })
-  tokenSchema.save()
-  const {params_token, cookie_token} = tokenSchema
-  res.cookie('token', cookie_token, {maxAge: 180000})
+      params_token: crypto.randomBytes(16).toString("hex"),
+      cookie_token: crypto.randomBytes(16).toString("hex"),
+    });
+    tokenSchema.save();
+    const { params_token, cookie_token } = tokenSchema;
+    res.cookie("token", cookie_token, { maxAge: 180000 });
 
-  console.log(`params_token: ${params_token}, cookie_token: ${cookie_token}`)
-  
-  
+    console.log(`params_token: ${params_token}, cookie_token: ${cookie_token}`);
+
     const output = `
   <p>${email_header}</p>
   <p>${msg}</p>
@@ -44,41 +45,42 @@ try{
     <li>email: ${email}</li>
     <li>Phone: ${tel}</li>
   <h1><a href="${authLinkHost}${authLink}/${params_token}/">Click here to authenticate your account</a></h1>
-  `
   
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_ADDRESS, // generated ethereal user
-      pass: process.env.EMAIL_PASS, // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-  
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"" <yujitest@test.com>', // sender address
-    to: email, // list of receivers
-    subject: "Node contact request ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: output, // html body
-  });
-  
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  res.json('Email has been sent')
-  }catch(e) {
-    console.log(e)
-    const user = await User.findOne({email: req.user.email})
-    const token = await Token.findOne({_userId: user._id})
-    await token.delete(()=>console.log('token has been deleted'))
-    await user.delete(() => console.log('user has been deleted'))
-    res.json({message:'email error', error: e})
-  }
-}
+  `;
 
-module.exports.sendVerifyEmail = sendVerifyEmail
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_ADDRESS, // generated ethereal user
+        pass: process.env.EMAIL_PASS, // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Bonbonmilano customer service" <yujitest@test.com>', // sender address
+      to: email, // list of receivers
+      subject: "Completa il registrazione", // Subject line
+      text: "Hello world?", // plain text body
+      html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    res.json("Email has been sent");
+  } catch (e) {
+    console.log(e);
+    const user = await User.findOne({ email: req.user.email });
+    const token = await Token.findOne({ _userId: user._id });
+    await token.delete(() => console.log("token has been deleted"));
+    await user.delete(() => console.log("user has been deleted"));
+    res.json({ message: "email error", error: e });
+  }
+};
+
+module.exports.sendVerifyEmail = sendVerifyEmail;
--- server.js
+++ server.js
@@ -1,74 +1,73 @@
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const path =require('path')
-const fs = require('fs')
-const mongoose =require('mongoose')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const cookieHandler = require('./middleware/cookieHandler')
-const methodOverride = require('method-override')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cookieHandler = require("./middleware/cookieHandler");
+const methodOverride = require("method-override");
 // const seedProduct = require('./seed/products')
 
+const blogRoutes = require("./routes/blog_routes");
+const shopRoutes = require("./routes/shop_routes");
+const cartRoutes = require("./routes/cart_routes");
+const authRoutes = require("./routes/auth_routes");
+const adminRoutes = require("./routes/admin_routes");
 
+const app = express();
 
-const blogRoutes = require('./routes/blog_routes')
-const shopRoutes = require('./routes/shop_routes')
-const cartRoutes = require('./routes/cart_routes')
-const authRoutes = require('./routes/auth_routes')
-const adminRoutes = require('./routes/admin_routes')
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
-const app = express()
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("DB OK"));
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(cookieHandler);
+app.use(methodOverride("_method"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, 
-  {useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true});
-  const db = mongoose.connection
-  
-  db.on('error', console.error.bind(console,'connection error:'))
-  db.once('open', ()=> console.log('DB OK'))
-  
-  mongoose.set('useFindAndModify', false)
-  
-  app.set('view engine', 'ejs')
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.use(cookieParser())
-  app.use(cookieHandler)
-  app.use(methodOverride('_method'))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
-  
-app.get('/', cookieHandler, (req, res) => {
-  const user = req.user
-  fs.readFile(path.join(__dirname, 'mainpage.json'), (err, data) => {
-  if(err)return res.sendStatus(500)
-  res.render('index', { 
-    user,
-    data: JSON.parse(data) })
-  
-  })
-})
+app.get("/", cookieHandler, (req, res) => {
+  const user = req.user;
+  fs.readFile(path.join(__dirname, "mainpage.json"), (err, data) => {
+    if (err) return res.sendStatus(500);
+    res.render("index", {
+      user,
+      data: JSON.parse(data),
+    });
+  });
+});
 
-app.get('/chisiamo', (req, res) =>{
-  res.render('chisiamo')
-})
-app.get('/nostroconcetti', (req, res) =>{
-  res.render('nostroconcetti')
-})
-app.get('/contatti', (req, res) =>{
-  res.render('contatti')
-})
+app.get("/chisiamo", (req, res) => {
+  res.render("chisiamo");
+});
+app.get("/nostroconcetti", (req, res) => {
+  res.render("nostroconcetti");
+});
+app.get("/contatti", (req, res) => {
+  res.render("contatti");
+});
 
-app.use('/blog', blogRoutes)
-app.use('/shop', shopRoutes)
-app.use('/cart', cartRoutes)
-app.use('/auth', authRoutes)
-app.use('/admin', adminRoutes)
+app.use("/blog", blogRoutes);
+app.use("/shop", shopRoutes);
+app.use("/cart", cartRoutes);
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
 // seedProduct()
 
-const PORT = process.env.PORT || 4242
+const PORT = process.env.PORT || 4242;
 
-app.listen(PORT, () => console.log(`server is runnning http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`server is runnning http://localhost:${PORT}`)
+);
